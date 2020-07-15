f4ade53a9fdafe2a56b5a5a994db642bddceffb8
Samuel Ramirez <samuel_ramirez@live.com.mx>
2020-07-14 20:57:38 -0500

    ejercicio terminado

---
+++ src/PrimeraApp.js
@@ -0,0 +1,26 @@
+// import React, { Fragment } from 'react';
+import React from 'react';
+import PropTypes from 'prop-types';
+
+// Functional component
+const PrimeraApp = ({ saludo, subtitulo }) => {
+
+  return (
+    <>
+      <h1> { saludo } </h1>
+      <p> { subtitulo } </p>
+    </>
+  );
+
+}
+
+
+PrimeraApp.propTypes = {
+  saludo: PropTypes.string.isRequired
+}
+
+PrimeraApp.defaultProps = {
+  subtitulo: 'Soy un subtitulo'
+}
+
+export default PrimeraApp;
